ae2e9bca7256951c0219d0a225fbf255d1481fdd
Shihaoooo <3141182493@qq.com>
2025-07-04 17:12:17 +0800

    create new entry
    new entry for new table

--- tlias-web-management/src/main/java/com/itheima/pojo/EmpExpr.java
+++ tlias-web-management/src/main/java/com/itheima/pojo/EmpExpr.java
@@ -1,7 +1,14 @@
 package com.itheima.pojo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDate;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class EmpExpr {
     private Integer id;
     private Integer empId;
